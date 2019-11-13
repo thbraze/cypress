3feb1e8bf36f353984e04add9fb69d75a8e8f46b
Thomas BRAZE <thomas.braze@opalesystems.com>
2019-11-13 12:48:54 +0100

    add logs

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -2,13 +2,17 @@
 describe('My First Test', function() {
     it('Test something', function() {
         cy.visit('https://example.cypress.io');
+        cy.log("url visited");
 
         cy.contains('type').click();
+        cy.log("type clicked");
 
         cy.url().should('include', '/commands/actions');
+        cy.log("new url valid");
 
         cy.get('.action-email')
             .type('fake@email.com')
             .should('have.value', 'fake@email.com');
+        cy.log("value is well prompted");
     });
 });
