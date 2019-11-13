0a0ad9408ec4fdbc0c2a78482890695b8b732133
Thomas BRAZE <thomas.braze@opalesystems.com>
2019-11-13 12:59:00 +0100

    add debugger

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -2,17 +2,17 @@
 describe('My First Test', function() {
     it('Test something', function() {
         cy.visit('https://example.cypress.io');
-        cy.log("url visited");
+        cy.log("url visited").debug();
 
         cy.contains('type').click();
-        cy.log("type clicked");
+        cy.log("type clicked").debug();
 
         cy.url().should('include', '/commands/actions');
-        cy.log("new url valid");
+        cy.log("new url valid").debug();
 
         cy.get('.action-email')
             .type('fake@email.com')
             .should('have.value', 'fake@email.com');
-        cy.log("value is well prompted");
+        cy.log("value is well prompted").debug();
     });
 });
