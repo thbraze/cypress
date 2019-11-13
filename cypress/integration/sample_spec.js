fab698f3804422c91666f16790e5ec12fe2ae85a
Thomas BRAZE <thomas.braze@opalesystems.com>
2019-11-13 12:41:12 +0100

    first commit

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,14 @@
+
+describe('My First Test', function() {
+    it('Test something', function() {
+        cy.visit('https://example.cypress.io');
+
+        cy.contains('type').click();
+
+        cy.url().should('include', '/commands/actions');
+
+        cy.get('.action-email')
+            .type('fake@email.com')
+            .should('have.value', 'fake@email.com');
+    });
+});
