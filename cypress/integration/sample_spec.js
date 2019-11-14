27eaa4c68a02ab3294eff18d9f09fc1603fcc11f
Thomas BRAZE <thomas.braze@opalesystems.com>
2019-11-14 10:01:43 +0100

    add a failing assertion

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -2,7 +2,6 @@
 context('Truc',() =>{
     beforeEach(()=>{
         cy.visit('https://example.cypress.io');
-        cy.log("url visited")
 
 
     })
@@ -10,15 +9,17 @@ context('Truc',() =>{
     it('Test something', () => {
 
         cy.contains('type').click();
-        cy.log("type clicked").debug();
+        cy.log("type clicked")
 
         cy.url().should('include', '/commands/actions');
-        cy.log("new url valid").debug();
+        cy.log("new url valid")
 
         cy.get('.action-email')
             .type('fake@email.com')
             .should('have.value', 'fake@email.com');
-        cy.log("value is well prompted").debug();
+        cy.log("value is well prompted")
+        let bool = true;
+        expect(bool).to.be.false;
     });
 })
 
